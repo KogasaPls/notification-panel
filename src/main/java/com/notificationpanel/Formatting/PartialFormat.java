d35440b25e2b170ce7c4f8e2b82735c3c41f1a06
Kogasa <kogasapls@gmail.com>
2022-12-10 15:25:08 -0600

    Remove unused constructor

--- src/main/java/com/notificationpanel/Formatting/PartialFormat.java
+++ src/main/java/com/notificationpanel/Formatting/PartialFormat.java
@@ -24,9 +24,6 @@ public class PartialFormat {
 
     public final List<FormatOption> options = new ArrayList<>();
 
-    private PartialFormat() {
-    }
-
     public PartialFormat(List<FormatOption> options) {
         for (FormatOption option : options) {
             mergeOption(option);
